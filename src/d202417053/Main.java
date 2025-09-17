8e2c29b315b304670d7d5f7b506c627239f52f85
Cho, Seoyun <whtjdbs3170@gmail.com>
2025-09-17 15:37:07 +0900

    six

--- src/d202417053/Main.java
+++ src/d202417053/Main.java
@@ -3,6 +3,7 @@ package d202417053;
 public class Main { 
 	public static void main(String[] args) { 
 		System.out.println("조서윤"); 
-		System.out.println("안녕하세요"); 
+		System.out.println("안녕하세요");
+		System.out.println("hello world");
 		} 
 	}
